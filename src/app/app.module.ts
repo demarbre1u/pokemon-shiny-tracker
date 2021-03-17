5dd4dcbf1344a7406c8cdf0be2cc5954c003f545
Demarbre Allan <demarbreallan@gmail.com>
2021-03-17 21:43:00 +0100

    Adding Material Selects, the new hunt modal now uses mat-select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SimpleModalModule } from 'ngx-simple-modal';
 import { NewHuntComponent } from './modal/new-hunt/new-hunt.component';
+
+import {MatSelectModule} from '@angular/material/select';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,10 @@ import { NewHuntComponent } from './modal/new-hunt/new-hunt.component';
   ],
   imports: [
     BrowserModule, 
+    BrowserAnimationsModule,
     ReactiveFormsModule, 
-    SimpleModalModule
+    SimpleModalModule, 
+    MatSelectModule
   ],
   providers: [],
   entryComponents: [
